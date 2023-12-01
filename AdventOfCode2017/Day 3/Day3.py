5fe2d1061e42f46201c9d6e3251a057cbfb08abf
Elizabeth Viera <elizabeth.viera.codes@gmail.com>
2023-11-30 21:40:47 -0800

    2021 Day 1

---
+++ AdventOfCode2017/Day 3/Day3.py
@@ -0,0 +1,23 @@
+top = 0
+bottom = 0
+left = 0
+right = 0
+
+target = 10 #312051
+
+dirs = [right, top, left, bottom]
+
+current_dir = 0
+i = 0
+total = 0
+while total < target:
+    while i < dirs[current_dir]:
+        i += 1
+        total += 1
+   
+    dirs[current_dir] = i
+    i = 0
+    current_dir += 1
+    current_dir %= 4
+
+print(top, bottom, left, right)
