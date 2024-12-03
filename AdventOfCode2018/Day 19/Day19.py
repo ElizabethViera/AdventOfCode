ef4c6e9672441966b7c43ceaeaf10286eee9e7b8
Elizabeth Viera <elizabeth.viera.codes@gmail.com>
2024-12-02 22:13:59 -0500

    2018 Day 19

--- AdventOfCode2018/Day 19/Day19.py
+++ AdventOfCode2018/Day 19/Day19.py
@@ -99,67 +99,56 @@ def eqrr(a, b, r, init):
     return init
 
 
-all_functions = [
-    addr,
-    addi,
-    mulr,
-    muli,
-    banr,
-    bani,
-    borr,
-    bori,
-    setr,
-    seti,
-    gtir,
-    gtri,
-    gtrr,
-    eqir,
-    eqri,
-    eqrr,
-]
+all_functions = {
+    "addr": addr,
+    "addi": addi,
+    "mulr": mulr,
+    "muli": muli,
+    "banr": banr,
+    "bani": bani,
+    "borr": borr,
+    "bori": bori,
+    "setr": setr,
+    "seti": seti,
+    "gtir": gtir,
+    "gtri": gtri,
+    "gtrr": gtrr,
+    "eqir": eqir,
+    "eqri": eqri,
+    "eqrr": eqrr,
+}
 
-function_map = dict()
 
-fileContents = open("AdventOfCode2018/Day 16/input.txt")
-cases = fileContents.read().split("\n\n")
+fileContents = open("AdventOfCode2018/Day 19/input.txt")
+arr = fileContents.read().split("\n")
 
-total = 0
-for case in cases:
-    init = case.split("\n")[0][:-1].split(": [")[1].split(", ")
-    init = [int(x) for x in init]
 
-    program = case.split("\n")[1]
-    program_contents = [int(x) for x in program.split(" ")]
-    p, a, b, r = (
-        program_contents[0],
-        program_contents[1],
-        program_contents[2],
-        program_contents[3],
-    )
+program: dict[int, list[str | int]] = dict()
+for i, line in enumerate(arr):
+    p, a, b, r = line.split()
+    a, b, r = int(a), int(b), int(r)
+    program[i] = [p, a, b, r]
 
-    result = case.split("\n")[2][:-1].split("[")[1].split(", ")
-    result = [int(x) for x in result]
+state = [1, 0, 0, 10551355, 0, 0]
+ip = 2
 
-    count = 0
-    for f in all_functions:
-        copy = deepcopy(init)
-        if f(a, b, r, copy) == result:
-            count += 1
-            resulting_f = f
-    if count == 1:
-        function_map[p] = resulting_f
-        all_functions.remove(resulting_f)
+counter = None
 
-programContents = open("AdventOfCode2018/Day 16/program.txt")
-program = programContents.read().split("\n")
+startPrinting = 40
 
-state = [0, 0, 0, 0, 0, 0]
-
-for ins in program:
-    print(ins)
-    instructions = [int(x) for x in ins.split(" ")]
-    p, a, b, r = instructions[0], instructions[1], instructions[2], instructions[3]
-    f = function_map[p]
+while True:
+    startPrinting -= 1
+    if startPrinting < 0:
+        break
+    # for ins in program:
+    if state[ip] not in program:
+        break
+    ins = program[state[ip]]
+    print(ins, state)
+    p, a, b, r = ins[0], ins[1], ins[2], ins[3]
+    p = str(p)
+    f = all_functions[p]
     state = f(a, b, r, state)
+    state[ip] += 1
 
 print(state)
